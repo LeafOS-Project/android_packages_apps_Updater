49db4c0f1049350d8c93529af3ab75bd862a8df1
Tim Zimmermann <tim@linux4.de>
2022-06-03 09:58:46 +0200

    Updater: Remove A/B performance mode setting
    Change-Id: I580cfa5893e0f9866d11139130bd3591dd5d805e

--- app/src/main/java/org/lineageos/updater/controller/ABUpdateInstaller.java
+++ app/src/main/java/org/lineageos/updater/controller/ABUpdateInstaller.java
@@ -207,10 +207,6 @@ class ABUpdateInstaller {
             }
         }
 
-        boolean enableABPerfMode = PreferenceManager.getDefaultSharedPreferences(mContext)
-                .getBoolean(Constants.PREF_AB_PERF_MODE, false);
-        mUpdateEngine.setPerformanceMode(enableABPerfMode);
-
         String zipFileUri = "file://" + file.getAbsolutePath();
         mUpdateEngine.applyPayload(zipFileUri, offset, 0, headerKeyValuePairs);
 
@@ -271,10 +267,6 @@ class ABUpdateInstaller {
                 .setStatus(UpdateStatus.INSTALLATION_CANCELLED);
         mUpdaterController.notifyUpdateChange(mDownloadId);
 
-    }
-
-    public void setPerformanceMode(boolean enable) {
-        mUpdateEngine.setPerformanceMode(enable);
     }
 
     public void suspend() {
