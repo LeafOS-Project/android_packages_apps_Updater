0ad2d8fbca70e56f9b1f2769ce5292d497c34e88
Erfan Abdi <erfangplus@gmail.com>
2022-07-22 17:02:22 +0430

    Updater: Redesign preferences dialog
    Co-authored-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: Ice1bc5ac5069e8954621ab6a6b27c757c53c4dc2

---
+++ src/org/lineageos/updater/ui/PreferenceSheet.kt
@@ -0,0 +1,85 @@
+package org.lineageos.updater.ui
+
+import android.annotation.SuppressLint
+import android.content.DialogInterface
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.SystemProperties
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Spinner
+import android.widget.Switch
+import android.widget.Toast
+import androidx.preference.PreferenceManager
+import org.lineageos.updater.R
+import org.lineageos.updater.UpdatesCheckReceiver
+import org.lineageos.updater.controller.UpdaterService
+import org.lineageos.updater.misc.Constants
+import org.lineageos.updater.misc.Utils
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+@SuppressLint("UseSwitchCompatOrMaterialCode")
+class PreferenceSheet : BottomSheetDialogFragment() {
+
+    private var prefs: SharedPreferences? = null
+
+    private var mUpdaterService: UpdaterService? = null
+    
+    private lateinit var preferencesAutoDeleteUpdates: Switch
+    private lateinit var preferencesMobileDataWarning: Switch
+    private lateinit var preferencesAutoUpdatesCheckInterval: Spinner
+
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.preferences_dialog, container, false)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(view) {
+            preferencesAutoDeleteUpdates = findViewById(R.id.preferences_auto_delete_updates)
+            preferencesMobileDataWarning = findViewById(R.id.preferences_mobile_data_warning)
+            preferencesAutoUpdatesCheckInterval = findViewById(R.id.preferences_auto_updates_check_interval)
+        }
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        preferencesAutoUpdatesCheckInterval.setSelection(Utils.getUpdateCheckSetting(requireContext()))
+        preferencesAutoDeleteUpdates.isChecked =
+            prefs!!.getBoolean(Constants.PREF_AUTO_DELETE_UPDATES, false)
+        preferencesMobileDataWarning.isChecked =
+            prefs!!.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true)
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        prefs!!.edit()
+            .putInt(
+                Constants.PREF_AUTO_UPDATES_CHECK_INTERVAL,
+                preferencesAutoUpdatesCheckInterval.selectedItemPosition
+            )
+            .putBoolean(Constants.PREF_AUTO_DELETE_UPDATES, preferencesAutoDeleteUpdates.isChecked)
+            .putBoolean(Constants.PREF_MOBILE_DATA_WARNING, preferencesMobileDataWarning.isChecked)
+            .apply()
+
+        if (Utils.isUpdateCheckEnabled(requireContext())) {
+            UpdatesCheckReceiver.scheduleRepeatingUpdatesCheck(requireContext())
+        } else {
+            UpdatesCheckReceiver.cancelRepeatingUpdatesCheck(requireContext())
+            UpdatesCheckReceiver.cancelUpdatesCheck(requireContext())
+        }
+
+        super.onDismiss(dialog)
+    }
+
+    fun setupPreferenceSheet(updaterService: UpdaterService): PreferenceSheet {
+        this.mUpdaterService = updaterService
+        return this
+    }
+}
